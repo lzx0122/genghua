2dc3d276cac29d58eb7729fee25637db8d5cd9cd
LZX <a0908287570@gmail.com>
2025-02-04 02:09:54 +0800

    新增 client

---
+++ client/src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import HelloWorld from "./views/AddUser.vue";
+import AddUser from "./views/AddUser.vue";
+</script>
+
+<template>
+  <div class="flex flex-col mx-auto w-full bg-white max-w-[480px]">
+    <div
+      class="flex gap-5 justify-between items-start pt-6 pr-2 pb-3 pl-6 w-full text-center whitespace-nowrap bg-white shadow-sm text-zinc-900"
+    >
+      <img
+        loading="lazy"
+        src="https://cdn.builder.io/api/v1/image/assets/TEMP/926d42507257ea90cd91fb72b17d6a1552c829ce2986fc4d8922969dc06d478d?placeholderIfAbsent=true&apiKey=1658e52805814928b66db3bf763c83c5"
+        class="object-contain shrink-0 mt-3.5 w-6 aspect-square"
+        alt=""
+      />
+      <div class="mt-4 text-2xl leading-none">新增顧客</div>
+      <div class="flex flex-col text-sm tracking-wide leading-none">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/27277a8596d4744ec4110b3d027c5c95bfd926359dabe4cc3a34fb01b3b7c10f?placeholderIfAbsent=true&apiKey=1658e52805814928b66db3bf763c83c5"
+          class="object-contain self-center w-6 aspect-square"
+          alt="User avatar"
+        />
+        <div>廖子翔</div>
+      </div>
+    </div>
+
+    <AddUser></AddUser>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ client/src/views/AddUser.vue
@@ -0,0 +1,102 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <div
+    class="overflow-hidden bg-white shadow-lg rounded-lg border p-2 cursor-grab active:cursor-grabbing"
+  >
+    <div class="flex flex-col px-6 mt-7 w-full">
+      <div class="self-start text-2xl leading-none text-zinc-900">
+        輸入顧客資訊
+      </div>
+      <form class="flex flex-col mt-3.5 w-full min-h-[622px]">
+        <div
+          class="flex justify-between items-start max-w-full text-lg leading-loose whitespace-nowrap text-zinc-900 w-[335px]"
+        >
+          <div class="flex flex-col rounded min-w-[240px] w-[335px]">
+            <label for="nameInput" class="self-start">名稱(可暱稱)</label>
+            <input
+              type="text"
+              id="nameInput"
+              placeholder="輸入名稱"
+              class="flex shrink-0 gap-2.5 py-3 mt-2 h-12 bg-white rounded border-2 border-solid border-zinc-900"
+            />
+          </div>
+        </div>
+        <div
+          class="flex flex-col mt-4 w-full text-lg leading-loose rounded max-w-[335px] text-zinc-900"
+        >
+          <label for="phoneInput" class="self-start">會員 (電話)</label>
+          <input
+            type="tel"
+            id="phoneInput"
+            placeholder="輸入電話"
+            class="flex shrink-0 gap-2.5 py-3 mt-2 h-12 bg-white rounded border-2 border-solid border-zinc-900"
+          />
+        </div>
+        <div class="flex flex-col mt-4 w-full rounded-lg max-w-[335px]">
+          <label
+            for="birthdateInput"
+            class="self-start text-lg leading-loose text-zinc-900"
+            >生日</label
+          >
+          <div
+            class="flex gap-2 items-start px-3.5 py-3 mt-2 bg-white rounded-lg border-2 border-solid border-zinc-900"
+          >
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/bc08cc744d03b4a6c40eab045ccabb44a2df07b744b8b72f5959183723232468?placeholderIfAbsent=true&apiKey=1658e52805814928b66db3bf763c83c5"
+              class="object-contain w-6 aspect-square"
+              alt="Calendar icon"
+            />
+            <input type="date" id="birthdateInput" class="w-full" />
+          </div>
+        </div>
+        <div
+          class="flex flex-col mt-4 w-full text-lg leading-loose whitespace-nowrap rounded max-w-[335px] text-zinc-900"
+        >
+          <label for="emailInput" class="self-start">電子信箱(可不填)</label>
+          <input
+            type="email"
+            id="emailInput"
+            placeholder="輸入信箱"
+            class="flex shrink-0 gap-2.5 py-3 mt-2 h-12 bg-white rounded border-2 border-solid border-zinc-900"
+          />
+        </div>
+        <div
+          class="flex flex-col mt-4 w-full text-lg leading-loose whitespace-nowrap rounded max-w-[335px] text-zinc-900"
+        >
+          <label for="notesInput" class="self-start">備註(可不填)</label>
+          <textarea
+            id="notesInput"
+            placeholder="輸入顧客的備註(顧客看不到)"
+            class="flex shrink-0 gap-2.5 mt-2 bg-white rounded border-2 border-solid border-zinc-900 h-[119px]"
+          ></textarea>
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <div
+    class="flex z-10 flex-col justify-center px-5 pt-6 pb-12 -mt-11 w-full text-2xl leading-none text-center text-white whitespace-nowrap bg-white shadow-sm"
+  >
+    <button
+      class="overflow-hidden gap-2.5 self-stretch px-6 py-3 rounded-lg border-4 border-solid bg-zinc-900 border-zinc-900"
+    >
+      新增
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
