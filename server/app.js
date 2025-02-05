99f74b00ad41d5a62580b8ac99e15b65df3626d6
Lzx0122 <a0908287570@gmail.com>
2025-02-05 23:03:26 +0800

    12

--- server/app.js
+++ server/app.js
@@ -11,27 +11,12 @@ require("dotenv").config();
 var app = express();
 
 app.use(
-  cors()
+  cors({
+    origin: ["https://genghua-coffee-app.vercel.app", "http://localhost:5173"], // 你的 Vue 前端網址
+    credentials: true, // 允許攜帶 Cookies
+  })
 );
-app.options("*", cors()); // 處理預檢請求
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://genghua-coffee-app.vercel.app"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  if (req.method === "OPTIONS") {
-    return res.status(204).end(); // 快速響應預檢請求
-  }
-
-  next();
-});
 app.use(cookieParser());
 
 app.use(logger("dev"));
