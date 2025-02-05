02d5b392a6b043f1ba8f72f4d61023cf49dc6ad5
LZX <a0908287570@gmail.com>
2025-02-05 21:42:50 +0800

    delete sameSite: "none"

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -40,7 +40,6 @@ router.post("/login", async function (req, res, next) {
         httpOnly: true,
         secure: true,
         maxAge: 3600000, // 1 小時
-        sameSite: "none", // 允許跨域
         path: "/", // 允許所有路徑存取
       });
 
