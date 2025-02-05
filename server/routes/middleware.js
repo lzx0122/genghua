5f90d520d06eaa40c0cc8d2fa8a68a0e5d311e74
Lzx0122 <a0908287570@gmail.com>
2025-02-05 23:54:12 +0800

    var jwt = require("jsonwebtoken");

--- server/routes/middleware.js
+++ server/routes/middleware.js
@@ -1,9 +1,9 @@
 var coffeModel = require("../models/").coffee;
+var jwt = require("jsonwebtoken");
 
 module.exports = {
   async verifyAdmin(req, res, next) {
     const token = req.cookies.genghua;
-    console.log(token);
     if (!token) return res.status(401).send("沒有登入");
     jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, user) => {
       console.log(user);
@@ -12,7 +12,6 @@ module.exports = {
       if (!r) {
         return res.status(401).send("驗證失敗");
       }
-
       req.user = user; // 將用戶資料附加到請求中
       next();
     });
